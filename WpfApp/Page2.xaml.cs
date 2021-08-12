095b990abd5cacf23cc23c7857429121b5cf2fdf
Den Pan <poquta@gmail.com>
2021-08-12 16:05:36 +0300

    Added page 2

---
+++ WpfApp/Page2.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Window
+    {
+        public Page2(Window mainPage)
+        {
+            InitializeComponent();
+            MainPage = mainPage;
+        }
+
+        private void btnNavigateMain_Click(object sender, RoutedEventArgs e)
+        {
+            MainPage.Show();
+            this.Close();
+        }
+
+        private Window MainPage { get; set; }
+    }
+}
